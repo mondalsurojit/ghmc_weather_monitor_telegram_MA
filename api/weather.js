9e7b6d3f33b6cbd30ab24463406e18a72395c3c4
surojitmondal <gobindomondal539@gmail.com>
2026-03-09 01:42:21 +0530

    minor fix

--- api/weather.js
+++ api/weather.js
@@ -1,7 +1,7 @@
 import { parseCSV } from '../src/utils/parseData.js'
 
 export default async function handler(req, res) {
-  const r = await fetch(import.meta.env.VITE_SHEET_URL)
+  const r = await fetch(process.env.VITE_SHEET_URL)
   const data = parseCSV(await r.text())
   res.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate=3600')
   res.status(200).json(data)
